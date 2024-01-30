917568ba8519da7b5fa57a5de358610055ba098d
ericzhou12 <erichyzhou@gmail.com>
2024-01-29 20:17:17 -0500

    Finished sidebar fully Created dashboard Working routing cards for each subsection Dash page finished Now open on conversation section and heading component NEXT - Finish conversation UI - Implement GPT API

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -4,8 +4,18 @@ import {Button} from "@/components/ui/button";
 import {Menu} from "lucide-react";
 import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
 import Sidebar from "./sidebar";
+import { useEffect, useState } from "react";
 
 const MobileSidebar = () => {
+    const [isMounted, setIsMounted] = useState(false);
+
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
+
+    if(!isMounted) {
+        return null;
+    }
     return(
         <Sheet>
             <SheetTrigger>
